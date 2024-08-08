3ced766ec2c20fa67de6bb6f653ea1da567d14f0
suresh.manivannan <suresh.manivannan@e-consystems.com>
2024-08-08 08:53:43 +0530

    allnew

--- src/Components/Hubpage Comp/Bloghubpages/Hubblogcard.jsx
+++ src/Components/Hubpage Comp/Bloghubpages/Hubblogcard.jsx
@@ -12,35 +12,20 @@ const Hubblogcard = () => {
       <div className="Hubblogcard-cards">
         <div className="Hubblogcard-card">
           <img src={Technical1} alt="AI-enabled point of care device" className="Hubblogcard-image" />
-<<<<<<< HEAD
           <h2 className="Hubblogcard-title">Building an AI-enabled point of care device</h2>
           <p className="Hubblogcard-text">Learn how e-con helped a leading US medical device manufacturer</p>
-=======
-          <h2 className="Hubblogcard-title">How to Overcome Imaging-Related Manufacturing Challenges of Dental Loupes</h2>
-          <p className="Hubblogcard-text">"The blog explores how overcoming issues like thermal dissipation, image stabilization...</p>
->>>>>>> 7f1d65ba117768d7073045fbb1a2011ce53268c6
           <AnimatedButton className="Hubblogcard-button1" text="Read more" backgroundColor="#344ea1" animationColor="#69ba2f" hoverColor="#69ba2f"></AnimatedButton>
         </div>
         <div className="Hubblogcard-card">
           <img src={Technical2} alt="AI-enabled point of care device" className="Hubblogcard-image" />
-<<<<<<< HEAD
           <h2 className="Hubblogcard-title">Building an AI-enabled point of care device</h2>
           <p className="Hubblogcard-text">Learn how e-con helped a leading US medical device manufacturer</p>
-=======
-          <h2 className="Hubblogcard-title">How ISPs Help Extraoral Cameras Significantly Improve Image Quality</h2>
-          <p className="Hubblogcard-text">ISPs enhance extraoral camera images by fine-tuning white balance, exposure, and more...</p>
->>>>>>> 7f1d65ba117768d7073045fbb1a2011ce53268c6
           <AnimatedButton className="Hubblogcard-button1" text="Read more" backgroundColor="#344ea1" animationColor="#69ba2f" hoverColor="#69ba2f"></AnimatedButton>
         </div>
         <div className="Hubblogcard-card">
           <img src={Technical3} alt="AI-enabled point of care device" className="Hubblogcard-image" />
-<<<<<<< HEAD
           <h2 className="Hubblogcard-title">Building an AI-enabled point of care device</h2>
           <p className="Hubblogcard-text">Learn how e-con helped a leading US medical device manufacturer</p>
-=======
-          <h2 className="Hubblogcard-title">Illumination vs. Image Quality: What Extraoral Cameras Really Need</h2>
-          <p className="Hubblogcard-text">Read about features like High Dynamic Range (HDR) and Dynamic Tone Mapping (DTM)...</p>
->>>>>>> 7f1d65ba117768d7073045fbb1a2011ce53268c6
           <AnimatedButton className="Hubblogcard-button1" text="Read more" backgroundColor="#344ea1" animationColor="#69ba2f" hoverColor="#69ba2f"></AnimatedButton>
         </div>
       </div>
